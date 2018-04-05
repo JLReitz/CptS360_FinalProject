6aabe7d4c282e383eff281732dd6303ffca4810c
Jensen Reitz <JLReitz@live.com>
2018-04-05 00:09:50 -0700

    Oops NOW I've included the file with said tokenizing function

---
+++ Util.c
@@ -0,0 +1,20 @@
+#inlcude "Type.h"
+
+// Prototypes *************************************************************************************
+
+int tokenize(char * path[], char * pathname, char * delimiter);
+
+// Funcions ***************************************************************************************
+
+int tokenize(char * path[], char * pathname, char * delimiter)
+{
+	//Set up
+	int tokens = 1;
+	path[tokens-1] = strtok(pathname, delimiter);
+	
+	//Loop iteratively until the pathname has been toklenized completely
+	while((tokens < BLKSIZE) && !(path[tokens-1] = strtok(0, delimiter)))
+		tokens++;
+	
+	return tokens;
+}
